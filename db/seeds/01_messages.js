5d62c7d7588b4e58473b9ee57ec329ee0ac91d67
Solarsine42 <Solarsine42@gmail.com>
2019-12-21 08:56:43 -0700

    fixed seed data

--- db/seeds/01_messages.js
+++ db/seeds/01_messages.js
@@ -6,14 +6,12 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex("messages").insert([
         {
-          id: 1,
           name: "Criminal",
           message: "What Are You?",
           created_at: "2016-06-26T14:26:16.000Z",
           updated_at: "2016-06-26T14:26:16.000Z"
         },
         {
-          id: 2,
           name: "Batman",
           message: "I'm Batman",
           created_at: "2016-06-26T14:26:16.000Z",
