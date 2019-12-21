7033f934ea7aa1d5b932a1cb6d9977f10baa4f67
Solarsine42 <Solarsine42@gmail.com>
2019-12-21 08:47:46 -0700

    second push

--- db/migrations/01_messages.js
+++ db/migrations/01_messages.js
@@ -1,6 +1,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("messages", function(table) {
-    table.increments();
+    table.increments().unique();
     table.string("name").notNullable();
     table.string("message").notNullable();
     table.timestamps(true, true);
